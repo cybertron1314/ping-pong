60013029ee59d57e5c294710075cdd02b918ff3e
Mark-43 <boyrsov13.oleg@gmail.com>
2026-05-03 13:13:40 +0300

    Update ooter_game.py
    изменил код

--- ooter_game.py
+++ ooter_game.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import randint
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_y, player_x, player_speed, size):
         super().__init__()
@@ -8,8 +9,10 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = player_y
         self.rect.x = player_x
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
@@ -17,27 +20,26 @@ class Player(GameSprite):
             self.rect.x += self.speed
         if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
-    def fire(self):
-        pass
 
 window = display.set_mode((700, 500))
-display.set_caption('Шутер')
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.set_volume(0.20)
-mixer.music.play()
-
+display.set_caption('Пинг Понг')
 
 clock = time.Clock()
 FPS = 60
 run = True
 finish = False
 
+font.init()
+my_font = font.SysFont("Arial", 35)
+win = my_font.render("Вы победили!", True, (255, 255, 0))
 
-    window.blit(background,(0, 0))
-    draw_lost(lost)
-    draw_score(0)
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    window.blit(background, (0, 0))
     if not finish:
-    
-    display.update() 
+        pass
+    display.update()
     clock.tick(60)
+
